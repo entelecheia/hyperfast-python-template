5ac99814c642b26050ac444619a1adf007155d85
Young Joon Lee <entelecheia@hotmail.com>
2023-06-13 09:14:03 +0900

    feat(cli): add click options and greet message

--- src/hyperfastpy/__cli__.py
+++ src/hyperfastpy/__cli__.py
@@ -2,13 +2,19 @@
 
 # Importing the libraries
 
+import click
+
 from . import __version__
 
 
-def main() -> None:
+@click.command()
+@click.version_option(__version__)
+@click.option("--count", "-c", default=1, help="Number of greetings.")
+@click.option("--name", "-n", prompt="Your name", help="The person to greet.")
+def main(count, name):
     """This is the cli function of the package"""
-    print("This is the cli function of the package")
-    print(f"The version of the package is: {__version__}")
+    for _ in range(count):
+        click.echo(f"Hello, {name}!")
 
 
 if __name__ == "__main__":
