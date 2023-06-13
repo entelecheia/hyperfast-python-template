c211b2ca3e5d3b6955e4d4e0a61c4a14ba5aa544
Young Joon Lee <entelecheia@hotmail.com>
2023-06-13 09:31:26 +0900

    chore(package): update get_version docstring

--- src/hyperfastpy/__init__.py
+++ src/hyperfastpy/__init__.py
@@ -2,5 +2,13 @@ from ._version import __version__
 
 
 def get_version() -> str:
+    """
+    Get the version of the package. This is the cli function of the package.
+    If you want to check the version of the package you can use this function.
+
+
+    Returns:
+        The version of the package as a string e. g. " 2. 0. 0 " or
+    """
     """This is the cli function of the package"""
     return __version__
